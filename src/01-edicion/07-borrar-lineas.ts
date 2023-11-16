b7b3cde9258b2b2d093d5336cd27abc6e3329fa8
Gilvan Costa <gilvancosta2025@gmail.com>
2023-11-16 16:29:32 -0300

    First Commit

---
+++ src/01-edicion/07-borrar-lineas.ts
@@ -0,0 +1,27 @@
+//*
+//*    Objetivo:
+//*        Comentar un bloque de código
+//*
+//*    Tips:
+//*        ⇧ ⌘ K
+//*        Ctrl + Shift + K
+//*
+//*    PRO:
+//*        Seleccionar todas las ocurrencias de la selección
+//*        ⇧ ⌘ L
+//*        Ctrl + Shift + L
+//*        Ctrl + Shift + K;
+//*
+
+let NoMeBorres = ':)';
+
+NoMeBorres = '1';
+NoMeBorres = '1';
+
+
+// Resultado final (sin los comentarios claro.)
+
+// let NoMeBorres = ':)';
+
+// NoMeBorres = '1';
+// NoMeBorres = '1';
